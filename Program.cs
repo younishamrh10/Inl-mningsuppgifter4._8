c0f6fb6367a00cf50a14d131bbb145f1a14a3239
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-10-23 15:32:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Inlämningsuppgifter4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv in en sträng: ");
+            string inputStr = Console.ReadLine();
+
+            if (inputStr.Length < 2)
+            {
+                Console.WriteLine("Strängen måste vara minst två tecken lång.");
+                return;
+            }
+
+            Console.Write("Skriv in de två tecknen (exempel: ac): ");
+            string charsToFind = Console.ReadLine();
+
+            if (charsToFind.Length != 2)
+            {
+                Console.WriteLine("Du måste skriva exakt två tecken.");
+                return;
+            }
+
+            char char1 = charsToFind[0];
+            char char2 = charsToFind[1];
+
+            if (inputStr.Contains(char1.ToString()) && inputStr.Contains(char2.ToString()))
+            {
+                Console.WriteLine($"Båda tecknen '{char1}' och '{char2}' finns i strängen.");
+            }
+            else
+            {
+                Console.WriteLine($"Minst ett av tecknen '{char1}' eller '{char2}' saknas i strängen.");
+            }
+        }
+    }
+}
